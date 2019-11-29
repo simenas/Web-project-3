c6533c596914cafabbaee1f007d99f9f2c19fe55
Stian <stianhn@gmail.com>
2019-11-29 23:35:37 +0100

    Fixed nav button open regardless of size

--- js/dashboard.js
+++ js/dashboard.js
@@ -1,7 +1,7 @@
 function openNav() {
   document.getElementById("mySidenav").style.width = "250px";
+  document.getElementById("mySidenav").style.display = "block";
 }
-
 function closeNav() {
   document.getElementById("mySidenav").style.width = "0";
 }
