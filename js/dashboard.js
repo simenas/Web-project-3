8cb682c9c87d1dceb7041fdebb5d95e44332993b
Stian <stianhn@gmail.com>
2019-11-30 20:41:59 +0100

    navbar fix

--- js/dashboard.js
+++ js/dashboard.js
@@ -2,13 +2,20 @@ function openNav() {
   document.getElementById("mySidenav").style.width = "250px";
   document.getElementById("mySidenav").style.display = "block";
   document.getElementById("container").style.opacity = "0.2";
+
+  if (window.matchMedia("(min-width: 850px)").matches) {
+    document.getElementById("container").style.opacity = "1";
+  }
 }
+
 function closeNav() {
   document.getElementById("mySidenav").style.width = "0";
   document.getElementById("container").style.opacity = "1";
 }
 
 function closeNav2() {
-  document.getElementById("mySidenav").style.width = "0";
-  document.getElementById("container").style.opacity = "1";
+  if (window.matchMedia("(max-width: 850px)").matches) {
+    document.getElementById("mySidenav").style.width = "0";
+    document.getElementById("container").style.opacity = "1";
+  }
 }
