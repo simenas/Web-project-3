b868d14b9b9ac9a70fa2eb4507e88bdb8e77a627
Stian <stianhn@gmail.com>
2019-11-30 15:11:20 +0100

    responsive sign in pages, opacity when navbar open

--- js/dashboard.js
+++ js/dashboard.js
@@ -1,7 +1,14 @@
 function openNav() {
   document.getElementById("mySidenav").style.width = "250px";
   document.getElementById("mySidenav").style.display = "block";
+  document.getElementById("container").style.opacity = "0.2";
 }
 function closeNav() {
   document.getElementById("mySidenav").style.width = "0";
+  document.getElementById("container").style.opacity = "1";
+}
+
+function closeNav2() {
+  document.getElementById("mySidenav").style.width = "0";
+  document.getElementById("container").style.opacity = "1";
 }
