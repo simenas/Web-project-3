c272d29241e782aaff65a90032c388e431169196
Stian <stianhn@gmail.com>
2019-12-07 04:35:09 +0100

    fixed logo, font on login, start to buttons

--- js/dashboard.js
+++ js/dashboard.js
@@ -19,3 +19,25 @@ function closeNav2() {
     document.getElementById("container").style.opacity = "1";
   }
 }
+
+/*Secures all alerts*/
+var elem = document.getElementById("alertBoxes");
+var secureAlerts = document.getElementById("myBtn");
+
+secureAlerts.addEventListener("click", mySecondFunction);
+
+function mySecondFunction() {
+  document.getElementById("securedAll").innerHTML = "SECURED";
+  elem.remove();
+}
+
+/*Secures alert1*/
+var alertBox1 = document.getElementById("alertBox1");
+var secureAlert1 = document.getElementById("secureAlert1");
+
+secureAlert1.addEventListener("click", removeAlertbox1);
+
+function removeAlertbox1() {
+  alertBox1.remove();
+  console.log("test");
+}
